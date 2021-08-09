eabca2f4dfa5e1647e921b622b874e51f7d9ab8b
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2021-08-09 14:16:35 -0400

    Numeros primos resolvidos

--- index.js
+++ index.js
@@ -21,12 +21,24 @@ function snapCrackle(maxValue) {
 function snapCracklePrime(maxValue) {
     let retorno = "";
     for (let max = 1; max <= maxValue; max++) {
+        if (numerosPrimos(max) === true && max % 2 !== 0 && max % 5 === 0) {
+            retorno += "SnapCracklePrime, ";
+            continue;
+        }
         if (max % 2 !== 0 && max % 5 === 0) {
             retorno += "SnapCrackle, ";
+            continue;
+        }
+        if (numerosPrimos(max) === true && max % 2 !== 0 && max > 1) {
+            retorno += "SnapPrime, ";
             continue;
         } 
         if (max % 2 !== 0) {
             retorno += "Snap, ";
+            continue;
+        }
+        if (numerosPrimos(max) === true && max % 5 === 0) {
+            retorno += "CracklePrime, ";
             continue;
         }
         if (max % 5 === 0) {
@@ -34,11 +46,22 @@ function snapCracklePrime(maxValue) {
             continue;
         }
 // Tentando resolver numeros primos!
-        if (max % max === 0) {
+        if (numerosPrimos(max) === true) {
             retorno += "Prime, ";
             continue;
         }
         retorno += max + ", ";
     }
     console.log(retorno);
+}
+
+// função para identificar se o numero é primo
+function numerosPrimos(num) {
+    let primo;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return primo = false
+        }
+    }
+    return primo = true
 }
